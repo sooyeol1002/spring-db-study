a22c238296e8cf5f45fdc902183cb8d082f91bee
Sooyeol Yang <didtnduf2000@gmail.com>
2023-07-31 19:21:38 +0900

    7월31일2차

---
+++ myapp/src/main/java/com/ysy/myapp/post/Post.java
@@ -0,0 +1,35 @@
+package com.ysy.myapp.post;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor // 전체 필드 초기 생성자
+@NoArgsConstructor  // 빈 생성자
+@Entity
+public class Post {
+    @Id
+    // database의 auto_increment를 사용함
+    // auto_increment: 레코드가 추가될때 자동으로 증가되는 값을 사용
+    // 1, 2, 3 ....
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long no;
+    private String title;
+    private String content;
+    private String creatorName;
+
+    // created_time bigint not null,
+    // primitive type: int, char, long, double
+    // nullable 불가, long 기본값이 0
+    // 데이터베이스에 NOT NULL로 세팅해줌
+//    private long createdTime;
+    // 데이터베이스에 NULL을 넣고 싶으면 Class 타입을 써야함
+    private Long createdTime;
+}
